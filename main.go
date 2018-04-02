2b764a4ec09e7a1505dc3af2162333115f5ce9bc
root <josh@cheetahfox.com>
2018-04-02 12:34:33 -0500

    Fetchpage is now returning two slices, one for names seeking and one for the jobs seeking

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func FetchPage(url string) {
+func FetchPage(url string) ([]string, []string) {
 	/* This funciton is rather specific to the page I am scraping. We take the url 
-	and return a 2d slice with players names and jobs that currently seeking 
+	and return two slices with players names and jobs that currently seeking 
 	party. 
 	*/
 	res, err := http.Get(url)
@@ -28,6 +28,9 @@ func FetchPage(url string) {
 		log.Fatal(err)
 	}
 
+	names := make([]string,0)
+	jobs  := make([]string,0)
+
 	doc.Find("td").Each(func(i int, s *goquery.Selection) {
 		// We skip the first two TD's because they don't contain normal data
 		if i == 1 {
@@ -37,17 +40,31 @@ func FetchPage(url string) {
 			return;
 		}
 		if (i-1)%4 == 0 {
-			fmt.Printf("index is %d, name is ----> %s\n", i, s.Text())
+			// x is the index in the data slice
+			x := (i-1)/4
+			fmt.Printf("index is %d, Slice Element is %d, name is ----> %s\n", i, x, s.Text())
+			names = append(names, s.Text())
 		}
 		if (i-2)%4 == 0 {
-			fmt.Printf("index is %d, job is ----> %s\n", i, s.Text())
+			x := (i-2)/4
+			fmt.Printf("index is %d, Slice Element is %d, job is ----> %s\n", i, x, s.Text())
+			jobs = append(jobs, s.Text())
 		}
 		})
+	return names, jobs
 }
 
 func main() {
+	names := make([]string,0)
+	jobs  := make([]string,0)
+
 	// Check that we have something in the command line
 	if len(os.Args) > 1 {
-		FetchPage(os.Args[1])
+		names, jobs = FetchPage(os.Args[1])
 	}
+	fmt.Printf("size of names %d\n",len(names))
+	fmt.Printf("size of jobs  %d\n",len(jobs))
+	/*for _, x := range names {
+		fmt.Printf(" Player: %s, is seeking on %s\n", names[x], jobs[x])
+	} */
 }
